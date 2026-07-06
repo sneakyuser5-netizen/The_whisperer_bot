1bfc3d8a8433785f1b5ff641dff163f4351a8f7f
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-06 22:35:31 +0100

    Update menu command to read commands from files
    Refactor menu command to dynamically list available commands from the commands directory.

--- commands/menu.js
+++ commands/menu.js
@@ -1,14 +1,34 @@
+const fs = require("fs");
+
 module.exports = {
     name: "menu",
-    execute: async (sock, msg) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text:
-`📌 MENU
 
-Commands:
-• hi
-• .ping
-• .menu`
-        });
+    description: "Show available commands",
+
+    category: "general",
+
+    execute: async (sock, msg) => {
+
+        const files = fs.readdirSync("./commands");
+
+        let menu = "📌 MENU\n\nCommands:\n";
+
+        for (const file of files) {
+
+            const command = require("./" + file);
+
+            if (command.name) {
+                menu += `• .${command.name}\n`;
+            }
+        }
+
+        menu += `\nTotal Commands: ${files.length}`;
+
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: menu
+            }
+        );
     }
-}
+};
