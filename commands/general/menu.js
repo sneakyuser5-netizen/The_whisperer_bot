c866bf9fd6f2024c3e3070d3ac57aabf6c3f6fbc
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-08 08:03:20 +0100

    Add files via upload

---
+++ commands/general/menu.js
@@ -0,0 +1,60 @@
+module.exports = {
+
+    name: "menu",
+
+    category: "general",
+
+    description: "Show bot commands",
+
+    permission: "public",
+
+    execute: async (sock, msg) => {
+
+        const { commands } = require("../../handler.js");
+
+        let menu = "📌 WhispererBot Menu\n";
+
+        const categories = {};
+
+        for (const [name, command] of commands.entries()) {
+
+            if (name !== command.name) continue;
+
+            const category = command.category || "other";
+
+            if (!categories[category]) {
+                categories[category] = [];
+            }
+
+            categories[category].push(command);
+        }
+
+
+        for (const category in categories) {
+
+            menu += `\n📂 ${category.toUpperCase()}\n`;
+
+            for (const command of categories[category]) {
+
+                menu += `• .${command.name}`;
+
+                if (command.description) {
+                    menu += `\n  ${command.description}`;
+                }
+
+                menu += "\n";
+            }
+        }
+
+
+        menu += `\nTotal Commands: ${commands.size}`;
+
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: menu
+            }
+        );
+
+    }
+};
