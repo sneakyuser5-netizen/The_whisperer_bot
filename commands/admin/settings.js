cfd3c730a0d4e2f41cc15dc31b0d41c35be0363e
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-08 20:40:21 +0100

    Update settings.js

--- commands/admin/settings.js
+++ commands/admin/settings.js
@@ -1,7 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-
-const file = path.join(__dirname, "../../database/settings.json");
+const settings = require("../../lib/settings");
 
 module.exports = {
 
