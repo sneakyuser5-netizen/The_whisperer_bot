e4481761906cf8a9f03e805d3e30145a96b159b6
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-09 09:07:53 +0100

    Create goodbye.js

---
+++ events/goodbye.js
@@ -0,0 +1,27 @@
+const settings = require("../lib/settings");
+
+module.exports = async (sock, update) => {
+
+    const group = update.id;
+
+    if (!settings.get(group).goodbye) {
+        return;
+    }
+
+    if (update.action !== "remove") {
+        return;
+    }
+
+    for (const user of update.participants) {
+
+        await sock.sendMessage(group, {
+            text:
+`👋 Goodbye @${user.split("@")[0]}
+
+We hope to see you again!`,
+            mentions: [user]
+        });
+
+    }
+
+};
