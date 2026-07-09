4b6c23fb8d9b15b23d040a134c1cfa223b24025c
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-09 21:11:04 +0100

    Create online.js

---
+++ commands/owner/online.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+    name: "online",
+
+    description: "Enable or disable always online",
+
+    category: "owner",
+
+    permission: "owner",
+
+    execute: async (sock, msg, args) => {
+
+        const jid = msg.key.remoteJid;
+
+        const option = args[0]?.toLowerCase();
+
+        if (!["on", "off"].includes(option)) {
+
+            return sock.sendMessage(jid, {
+                text: "Usage:\n.online on\n.online off"
+            });
+
+        }
+
+
+        if (option === "on") {
+
+            await sock.sendPresenceUpdate(
+                "available"
+            );
+
+            await sock.sendMessage(jid, {
+                text: "✅ Always online enabled."
+            });
+
+        } else {
+
+            await sock.sendPresenceUpdate(
+                "unavailable"
+            );
+
+            await sock.sendMessage(jid, {
+                text: "✅ Always online disabled."
+            });
+
+        }
+
+    }
+
+};
