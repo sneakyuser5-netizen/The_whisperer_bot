de0bd460ef0a970187c5ebce0a3a08d04415390d
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-10 16:44:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,67 @@ async function startBot() {
             printQRInTerminal: false,
             logger: pino({ level: "silent" })
         });
+        const originalSendMessage = sock.sendMessage.bind(sock);
+
+sock.sendMessage = async (jid, content, options) => {
+
+    try {
+
+        const config = settings.get("global");
+
+        if (config.autotyping) {
+
+            console.log("⌨️ AUTO TYPING");
+
+            await sock.sendPresenceUpdate(
+                "composing",
+                jid
+            );
+
+            await new Promise(resolve =>
+                setTimeout(resolve, 5000)
+            );
+
+            await sock.sendPresenceUpdate(
+                "available",
+                jid
+            );
+
+        }
+
+        else if (config.autorecording) {
+
+            console.log("🎙️ AUTO RECORDING");
+
+            await sock.sendPresenceUpdate(
+                "recording",
+                jid
+            );
+
+            await new Promise(resolve =>
+                setTimeout(resolve, 5000)
+            );
+
+            await sock.sendPresenceUpdate(
+                "available",
+                jid
+            );
+
+        }
+
+    } catch (err) {
+
+        console.log("PRESENCE ERROR:", err);
+
+    }
+
+    return originalSendMessage(
+        jid,
+        content,
+        options
+    );
+
+};
 
         sock.ev.on("creds.update", saveCreds);
 
