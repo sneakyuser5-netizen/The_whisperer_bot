139ec4a819fac87efd85a76421b3081fd30c700e
The-whisperer-237 <thewhisperer056@gmail.com>
2026-07-15 18:13:06 +0100

    Add check for existing WhisperBot process

--- start.sh
+++ start.sh
@@ -4,4 +4,12 @@ echo "🚀 Starting WhisperBot..."
 
 npm install pm2 --save
 
-npx pm2 start index.js --name whisperbot --attach
+npx pm2 describe whisperbot >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "♻️ Restarting existing WhisperBot..."
+    npx pm2 restart whisperbot --attach
+else
+    echo "✨ Starting WhisperBot for the first time..."
+    npx pm2 start index.js --name whisperbot --attach
+fi
