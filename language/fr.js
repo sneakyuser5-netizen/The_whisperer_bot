d24b199ad5a2b99fc730a361c7b3356eaae323e2
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-07-25 18:54:28 +0100

    Update fr.js

--- language/fr.js
+++ language/fr.js
@@ -1,6 +1,7 @@
 module.exports = {
     menu_title: "📌 Menu WhisperBot",
     total_commands: "Nombre total de commandes",
+    language: "Changer la langue du bot. Ex: .language fr"
     gstatus: "Afficher le statut des fonctionnalités du groupe.",
 //language: "Changer la langue du bot. Ex: .language fr"
     "owner.language_usage":
