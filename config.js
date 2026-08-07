42ca76bc5cc1717004bcc7cc29b32c1f485cc7d9
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-08-07 02:07:28 +0100

    Update BOT_NAME to use settings from lib

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BOT_NAME: "Whisperer_Bot",
+    BOT_NAME: require("./lib/settings").get("global").bot_name || "Whisperer_Bot",
     PREFIX: ".",
     CREATOR: "237682349827",
     TIMEZONE: "Africa/Douala"
