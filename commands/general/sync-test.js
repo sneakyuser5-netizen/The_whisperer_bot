6b38b2cb3e68965409242bf49f1cb384417d93fb
sneakyuser5-netizen <sneakyuser5@gmail.com>
2026-08-15 21:23:18 +0100

    Delete commands/general/sync-test.js

--- commands/general/sync-test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "sync-test",
-    description: "Test automatic source synchronization",
-    execute: async ({ sock, msg }) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: "✅ Sync test works! This command came from the latest GitHub source."
-        });
-    }
-};
